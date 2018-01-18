4c3912c8b0d5bc4a8c2670acb3658fe4daef3f08
wangdian1167 <wangdian1167@gmail.com>
2018-01-18 23:11:29 +0800

    1

--- Game_Sandbox/test02.py
+++ Game_Sandbox/test02.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-Created on Wed Jan 17 16:29:18 2018
+Created on Thu Jan 18 19:24:55 2018
 
-1 vs 1
+N vs N
 按水平分配输赢概率
 输赢升降段位
 
@@ -26,28 +26,25 @@ D_players = {} # 玩家字典
 N_players = 100 # 玩家数量
 Rank_max = 200 # 最高段位
 
+# 设置玩家水平
+Ability_init = np.random.randn(N_players)
+Ability_int = np.around((np.abs(Ability_init) + 1)**2)
+# 设置玩家活跃度
+
+
 # =============================================================================
 # 添加新玩家
 # =============================================================================
 def add_players(N_players_add):
-    
-    # 设置玩家水平
-#    Ability_init = np.random.randn(N_players_add)
-#    Ability_0 = np.around((np.abs(Ability_init) + 1)**2)
-    # 设置玩家天赋
-    Talent_init = np.random.randn(N_players_add)
-    Talent_0 = ((np.abs(Talent_init) + 2)/2)
-    N_players_now = len(D_players)
-    for i in range(N_players_now, N_players_now + N_players_add):
+    for i in range(len(D_players), len(D_players) + N_players_add):
         D_players[i]={}
-        D_players[i]['Money'] = 1000  # 初始金钱
+        D_players[i]['Money'] = 100  # 初始金钱
         
     #    D_players[i]['Ability'] = int(Ability_int[i]) # 玩家水平
         D_players[i]['Ability'] = 2 # 初始玩家水平
         
         D_players[i]['Activity'] = 1 # 活跃度
         D_players[i]['Lucky'] = 0.5 # 幸运值
-        D_players[i]['Talent'] = Talent_0[i - N_players_now] # 天赋值
         
         D_players[i]['win_times'] = 0  # 初始化 胜利次数
         D_players[i]['lose_times'] = 0 # 初始化 失败次数
@@ -78,7 +75,7 @@ def fight(p1, p2):
         # =====================================================================
         #  赢家
         # =====================================================================
-        D_players[p_win]['Money'] += 10   # 赢家获利
+#        D_players[p_win]['Money'] += 10   # 赢家获利
         D_players[p_win]['win_times'] += 1 # 胜利次数+1
         D_players[p_win]['game_times'] += 1 # 游戏次数+1
         D_players[p_win]['game_history'].append((p_win, p_lose, 'win')) # 添加进游戏历史
@@ -95,11 +92,11 @@ def fight(p1, p2):
             D_players[p_win]['Rank'] = Rank_max
         
         # 玩家水平
-        D_players[p_win]['Ability'] += 1 * D_players[p_win]['Talent'] # 玩家水平+
+        D_players[p_win]['Ability'] += 1 # 玩家水平+
         # =====================================================================
         #  输家
         # =====================================================================        
-        D_players[p_lose]['Money'] -= 9   # 输家失利
+#        D_players[p_lose]['Money'] -= 9   # 输家失利
         D_players[p_lose]['lose_times'] += 1 # 胜利次数+1
         D_players[p_lose]['game_times'] += 1 # 游戏次数+1
         D_players[p_lose]['game_history'].append((p_lose, p_win, 'lose')) # 添加进游戏历史
@@ -112,7 +109,7 @@ def fight(p1, p2):
             D_players[p_lose]['Rank'] -= 1 # 段位减少
             
         # 玩家水平
-        D_players[p_lose]['Ability'] += 0.2 * D_players[p_lose]['Talent'] # 玩家水平+
+        D_players[p_lose]['Ability'] += 0.2 # 玩家水平+
         return True
     else:
         return False
@@ -157,7 +154,6 @@ for i in range(15):
 # =============================================================================
 # 统计数据
 # =============================================================================
-Talent = []
 Ability = []
 Money = []
 Rank = []
@@ -166,7 +162,6 @@ win_times = []
 lose_times = []
 game_times = []
 for i in D_players.keys():
-    Talent.append(D_players[i]['Talent'])
     Ability.append(D_players[i]['Ability'])
     Money.append(D_players[i]['Money'])
     Rank.append(D_players[i]['Rank'])
@@ -179,48 +174,49 @@ Players = list(D_players.keys())
 print('总游戏次数：', Game_Times)
 # 总矩阵
 A = np.concatenate((np.array(Players)[:, np.newaxis], # 0 玩家名
-                    np.array(Talent)[:, np.newaxis], # 1 玩家天赋
-                    np.array(Ability)[:, np.newaxis], # 2 玩家水平
-                    np.array(Money)[:, np.newaxis], # 3 金币
-                    np.array(Rank)[:, np.newaxis], # 4 段位
-                    np.array(winning_streak_max)[:, np.newaxis], # 5 最高连胜
-                    np.array(win_times)[:, np.newaxis], # 6 胜场数
-                    np.array(lose_times)[:, np.newaxis], # 7 败场数
-                    np.array(game_times)[:, np.newaxis]), axis = 1) # 8 总游戏次数
+                np.array(Ability)[:, np.newaxis], # 1 玩家水平
+                np.array(Money)[:, np.newaxis], # 2 金币
+                np.array(Rank)[:, np.newaxis], # 3 段位
+                np.array(winning_streak_max)[:, np.newaxis], # 4 最高连胜
+                np.array(win_times)[:, np.newaxis], # 5 胜场数
+                np.array(lose_times)[:, np.newaxis], # 6 败场数
+                np.array(game_times)[:, np.newaxis]), axis = 1) # 7 总游戏次数
 
 # =============================================================================
 # 画图
 # =============================================================================
 #plt.figure()
-#plt.hist(A[:, 4], bins=50)
+#plt.hist(A[:, 3], bins=50)
 #plt.show()
 
-#A_filted = A[np.where(np.array(A[:, 4])>0)] # 按段位筛选
+#A_filted = A[np.where(np.array(A[:, 3])>0)] # 按段位筛选
 
 #plt.figure()
 #plt.scatter(Ability, Money)
 #plt.show()
 
 #plt.figure()
-#plt.hist(A_filted[:, 4], bins=30)
+#plt.hist(A_filted[:, 3], bins=30)
 #plt.show()
 
-win_rate = A[:, 6]/A[:, 8]
+#plt.scatter(Rank, Money)
+win_rate = A[:, 5]/A[:, 7]
+
+plt.figure()
+plt.hist(win_rate)
+plt.show()
+
+plt.figure()
+plt.scatter(A[:, 1], win_rate)
+plt.show()
 
 print('游戏次数-水平')
 plt.figure()
-plt.scatter(A[:, 8], A[:, 2])
+plt.scatter(A[:, 7], A[:, 1])
 plt.show()
 
-print('天赋-水平')
-plt.figure()
-plt.scatter(A[:, 1], A[:, 2])
-plt.show()
 
-print('天赋-段位')
-plt.figure()
-plt.scatter(A[:, 1], A[:, 4])
-plt.show()
+
 
 
 
